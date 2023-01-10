f6fb64335fddae37761720244af7c9370e3ff958
Dalitso Kasonde <dkasonde11@gmail.com>
2023-01-10 11:51:13 +0200

    create tweet outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Hello from "./sayHello";
+import Tweet from "./Tweet";
 
 function App() {
   return (
-    <div>
-      <h1>This is the App Component</h1>
-      <Hello />
+    <div className="app">
+      <h1>Hello React</h1>
+      <Tweet />
     </div>
   );
 }
---
+++ src/Tweet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+
+function Tweet(){
+    return(
+        <div className="tweet">
+            <h3>Name</h3>
+            <p>This is a random Tweet</p>
+            <h3>Number of Likes</h3>
+        </div>
+    )
+}
+
+export default Tweet;
