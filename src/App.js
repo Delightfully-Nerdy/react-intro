87320bf52f70f6d6ac6ae6314840ba223e0b7708
Dalitso Kasonde <dkasonde11@gmail.com>
2023-01-10 12:22:32 +0200

    add: set count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Tweet from "./Tweet";
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="app">
-      <Tweet name = "Dalitso" message="This is a random tweet" likes="2"/>
-      <Tweet name = "Janet" message=" I love life" likes="0"/>
-      <Tweet name = "Bernadette" message="Thiss i a random tweet" likes="5"/>
-      <Tweet name = "Elizabeth" message="This is a tweet" likes="22"/>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
