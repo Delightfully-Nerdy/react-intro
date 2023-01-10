8cd49d7aee3a638ee9f5332bd5abdd48b032f289
Dalitso Kasonde <dkasonde11@gmail.com>
2023-01-10 12:07:36 +0200

    fix: isolate properties

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <Tweet name = "Dalitso" message="This is a random tweet" likes="2"/>
-      <Tweet name = "Janet" message=" is a random tweet" likes="0"/>
+      <Tweet name = "Janet" message=" I love life" likes="0"/>
       <Tweet name = "Bernadette" message="Thiss i a random tweet" likes="5"/>
       <Tweet name = "Elizabeth" message="This is a tweet" likes="22"/>
     </div>
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './App.css'
 
-function Tweet(props){
+function Tweet({name, message, likes}){
     return(
         <div className="tweet">
-            <h3>{props.name}</h3>
-            <p>{props.message}</p>
-            <h3>{props.likes}</h3>
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>{likes}</h3>
         </div>
     )
 }
