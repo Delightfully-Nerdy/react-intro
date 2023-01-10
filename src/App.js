baaac4100143d07bc9271dcbde889a1f274221d1
Dalitso Kasonde <dkasonde11@gmail.com>
2023-01-10 11:54:57 +0200

    mutiple components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Tweet from "./Tweet";
 function App() {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      <Tweet />
+      <Tweet />
+      <Tweet />
       <Tweet />
     </div>
   );
